7feceabd69e9eb5afe55bc28a0404f25e622b182
Greg Schulte <rikkitikkitumbo@gmail.com>
2014-06-23 18:15:25 -0700

    3rd commit

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,7 @@
 class Pet < ActiveRecord::Base
   belongs_to :pet_store
+
+
   scope :red, -> {where(color: "red")}
   scope :badger, -> {where(species: "badger")}
   default_scope :order => 'LOWER(name) ASC'
@@ -10,18 +12,9 @@ class Pet < ActiveRecord::Base
   validates :name, :species, :color, :pet_store_id, presence: true
   validates :species, inclusion: { in: ["dog", "cat", "goldfish", "gold-fish", "gold fish", "dragon" ], message: "we don't sell that pet!"}
   validates :color, exclusion: { in: [ "blue" ], message: "blue doesn't look good on pets, or on the website"}
-  #validate :validate_color
-  #validate :validate_species
-  validate :pet_store, :associated => true
-  validate :validate_store_id
 
-  private
-    def validate_store_id
-      # errors.add(:pet_store_id, "is invalid") unless PetStore.exists?(pet_store_id)
-      if !PetStore.exists?(pet_store_id)
-        errors.add(:pet_store_id, "invalid pet store")
-      end
-    end
+  validates :pet_store, :presence => {message: "invalid store id"}
 
 
 end
+
